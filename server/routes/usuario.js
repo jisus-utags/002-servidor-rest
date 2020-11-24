8559862fa3b4e9f50b77c343c88c5755ba3c9de4
Jose de Jesus Estrada Guevara <guevarajesus632@gmail.com>
2020-11-24 16:31:14 -0600

    Creacion de una carpeta de rutas

---
+++ server/routes/usuario.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express ();
+
+app.get('/usuario', function (req, res){
+    res.json({
+        ok: 200,
+        mensaje: 'Usuarios consultados con exito'
+    })
+});
+
+app.get('/saludo', function (req, res){
+    res.json({
+        ok: 200,
+        mensaje: 'Bienvenido Jose'
+    })
+});
+
+app.post('/usuario', function(req,res){
+    let nombre = req.body.nombre;
+    let body = req.body;
+
+    if(nombre == undefined){
+        res.status(400).json({
+            ok: 400,
+            mensaje: 'Por favor de enviar el valor del nombre'
+        });   
+    }else{
+        res.json({
+            ok: 200,
+            mensaje: 'usuario insertado con exito',
+            body: body
+        });
+    };
+
+    res.json({
+        ok: 200,
+        mensaje: 'usario insertado con exito',
+        body: body
+    })
+});
+
+app.put('/usuario/:id/:nombre', function(req,res){
+    let id = req.params.id;
+    let nombre = req.params.nombre;
+
+    res.json({
+        ok: 200,
+        mensaje: 'Usuario actualizado con exito',
+        id: id,
+        nombre: nombre
+    });
+});
+
+app.delete('/usuario/:id', function(req,res){
+    let id = req.params.id;
+
+    res.json({
+        ok: 200,
+        mensaje: 'Usuario eliminado con exito',
+        id: id
+    });
+});
+
+module.exports = app;
